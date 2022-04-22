8ce56debb6d9a28cf4815cd610c8bc33330960ae
zcomert <zafer.comert@hotmail.com>
2022-04-22 12:10:32 +0300

    Entities project has been added to the project.

---
+++ ProjectManagement/Controllers/ProjectsController.cs
@@ -0,0 +1,28 @@
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectManagement.Controllers
+{
+    [Route("api/projects")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        private List<Project> _projectList;
+        public ProjectsController()
+        {
+            _projectList = new List<Project>
+            {
+                new Project { Id = Guid.NewGuid(), Name ="Project 1"},
+                new Project { Id = Guid.NewGuid(), Name ="Project 2"},
+                new Project { Id = Guid.NewGuid(), Name ="Project 3"}
+            };
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_projectList);
+        }
+    }
+}
