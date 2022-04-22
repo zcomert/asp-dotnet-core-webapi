44fe53c609332c4dec3fdf1bdc63f1f06e903fb5
zcomert <zafer.comert@hotmail.com>
2022-04-22 14:56:06 +0300

    RepositoryBase hsa been implemented.

---
+++ Repository/RepositoryBase.cs
@@ -0,0 +1,39 @@
+﻿using Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected RepositoryContext _repositoryContext;
+
+        protected RepositoryBase(RepositoryContext repositoryContext)
+        {
+            _repositoryContext = repositoryContext;
+        }
+
+        public void Create(T entity) => _repositoryContext.Set<T>().Add(entity);
+        public void Delete(T entity) => _repositoryContext.Set<T>().Remove(entity);
+
+        public IQueryable<T> FindAll(bool trackChanges) =>
+            !trackChanges ?
+            _repositoryContext.Set<T>().AsNoTracking() :
+            _repositoryContext.Set<T>();
+
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges) =>
+            !trackChanges ?
+            _repositoryContext.Set<T>().Where(expression).AsNoTracking() :
+            _repositoryContext.Set<T>();
+
+
+        public void Update(T entity) => _repositoryContext.Set<T>().Update(entity);
+
+    }
+}
